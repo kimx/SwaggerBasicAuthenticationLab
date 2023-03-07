a89f1cb4d5eb87ee22cf5c3f0a062b074b2436df
kim <kim@bud4.net>
2023-03-07 14:55:03 +0800

    init

---
+++ SwaggerBasicAuthenticationLab/BasicAuthenticationHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text;
+
+namespace SwaggerBasicAuthenticationLab;
+
+/// <summary>
+/// 參考來源:https://www.c-sharpcorner.com/article/basic-authentication-in-swagger-open-api-net-5/
+/// 1.實作AuthenticationHandler
+/// </summary>
+public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+
+
+    #region Constructor  
+    public BasicAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    {
+    }
+    #endregion
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var authorization = Request.Headers["Authorization"];
+        if (string.IsNullOrEmpty(authorization))
+            return AuthenticateResult.Fail($"Authentication failed: Authorization empty");
+        var authHeader = AuthenticationHeaderValue.Parse(authorization);
+        var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter)).Split(':');
+        string username = credentials.FirstOrDefault();
+        var password = credentials.LastOrDefault();
+        var result = username == "kim" && password == "kim123";
+        if (!result)
+            return AuthenticateResult.Fail($"Authentication failed: Invalid credentials");
+
+        var claims = new[] {
+                new Claim(ClaimTypes.Name, username)
+            };
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+        return AuthenticateResult.Success(ticket);
+    }
+
+}
---
+++ SwaggerBasicAuthenticationLab/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
+using SwaggerBasicAuthenticationLab;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//3.Swagger加上Basic驗證
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BasicAuth", Version = "v1" });
+    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = ParameterLocation.Header,
+        Description = "Basic Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "basic"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+});
+
+//4.加入驗證處理者
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
