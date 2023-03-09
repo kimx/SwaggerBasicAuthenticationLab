2df5243150588ab97870a46b552e00085ef7cd70
kim <kim@bud4.net>
2023-03-09 15:38:11 +0800

    SwaggerBasicAuthenticationLab.ConsoleApp 加入NSwag Client產生器

---
+++ SwaggerBasicAuthenticationLab.ConsoleApp/Clients/BaseClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwaggerBasicAuthenticationLab.ConsoleApp.Clients
+{
+    public class BaseClient
+    {
+        public BaseClient(ClientConfiguration configuration)
+        {
+
+        }
+    }
+}
